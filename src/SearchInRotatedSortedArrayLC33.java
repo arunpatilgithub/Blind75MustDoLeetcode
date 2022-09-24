e97db75d33f5c706e2dadc8739683bfe678ac8ed
arunpatil <arunpatilmailbox@gmail.com>
2022-09-24 08:07:16 -0600

    Solution to LC33 with recursion

--- src/SearchInRotatedSortedArrayLC33.java
+++ src/SearchInRotatedSortedArrayLC33.java
@@ -6,17 +6,19 @@ public class SearchInRotatedSortedArrayLC33 {
     public static void main(String[] args) {
 
         //int[] nums = {4, 5, 6, 7, 0, 1, 2};
-        int[] nums = {5, 1};
-        int target = 5;
+        //int[] nums = {5, 1};
+        //int[] nums = {5, 3};
+        int[] nums = {1};
+        int target = 3;
 
         System.out.println(search(nums, target));
     }
 
-    private static int search(int[] nums, int target) {
+    private static int searchIterative(int[] nums, int target) {
 
         int left = 0;
         int right = nums.length - 1;
-        
+
         while (left <= right) {
 
             int mid = (left + right) / 2;
@@ -44,6 +46,43 @@ public class SearchInRotatedSortedArrayLC33 {
         }
 
         return -1;
+    }
+
+    private static int search(int[] nums, int target) {
+        return searchRecursion(nums, 0, nums.length - 1, target);
+    }
+
+    private static int searchRecursion(int[] nums, int left, int right, int target) {
+
+        if (left > right) {
+            return -1;
+        }
+
+        int mid = (left + right) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        if (nums[left] <= nums[mid]) {
+
+            if (target >= nums[left] && target <= nums[mid]) {
+                right = mid - 1;
+                return searchRecursion(nums, left, right, target);
+            } else {
+                left = mid + 1;
+                return searchRecursion(nums, left, right, target);
+            }
+
+        } else {
+            if (target >= nums[mid + 1] && target <= nums[right]) {
+                left = mid + 1;
+                return searchRecursion(nums, left, right, target);
+            } else {
+                right = mid - 1;
+                return searchRecursion(nums, left, right, target);
+            }
+        }
     }
 
 }
