5f5db2415c458374c0329a7b2a573e3634477dbd
arunpatil <arunpatilmailbox@gmail.com>
2022-09-25 10:46:42 -0600

    Solution to LC48-No transpose and reverse

--- src/RotateImageLC48.java
+++ src/RotateImageLC48.java
@@ -27,6 +27,21 @@ public class RotateImageLC48 {
         }
     }
 
+    public static void rotate2(int[][] matrix) {
+
+        int n = matrix.length, temp;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - i - 1; j++) {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = temp;
+            }
+        }
+    }
+
+
     public static void rotate(int[][] matrix) {
 
         //Step1: transpose
