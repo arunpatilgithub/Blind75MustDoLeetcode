b6ab977263b26385df4fe754cfbba6efa6b1459c
arunpatilgithub <arunpatilmailbox@gmail.com>
2022-09-26 15:02:37 -0600

    Testing checking from multiple github setups

--- src/JumpGameLC55.java
+++ src/JumpGameLC55.java
@@ -6,8 +6,8 @@ public class JumpGameLC55 {
 
     public static void main(String[] args) {
 
-        //int[] nums = {2,3,1,1,4};
-        int[] nums = {3,2,1,0,4};
+        int[] nums = {2,3,1,1,4};
+        //int[] nums = {3,2,1,0,4};
         //int[] nums = {0};
 
         System.out.println(canJump(nums));
